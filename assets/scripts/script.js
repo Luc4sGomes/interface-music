21c59ed83f9b9bf985a45c89b570b6e6064e1e7e
lucas <lucasgominhaa448@gmail.com>
2021-08-04 00:30:45 -0300

    added changes

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,3 @@
 const form = document.querySelector('#form');
 const inputEmail = document.querySelector('')
 
-form.addEventListener('submit',function(event){
-    event.preventDefault();
-    verifyUser();
-});
