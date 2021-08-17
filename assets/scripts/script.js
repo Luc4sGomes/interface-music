74d3bd090744ffdb152d95cc0d15144940a4b491
lucas <lucasgominhaa448@gmail.com>
2021-08-16 22:05:52 -0300

    added fetch in script js that takes data from json file anda compare with the users data

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,25 +1,51 @@
-const form = document.querySelector('#form');
+const form = document.querySelector("#form");
 const bolhas = document.querySelector("ul.bolhas");
 
-for(let i = 0; i < 10; i++){
-    const li = document.createElement("li");
+for (let i = 0; i < 10; i++) {
+  const li = document.createElement("li");
 
-    const random = (min,max) => Math.random() * (max - min) + min;
-    const size = Math.floor(random(10,120));
-    const position = random(1,99);
-    const delay = random(5,0.1);
-    const duration = random(24,12);
+  const random = (min, max) => Math.random() * (max - min) + min;
+  const size = Math.floor(random(10, 120));
+  const position = random(1, 99);
+  const delay = random(5, 0.1);
+  const duration = random(24, 12);
 
-    li.style.width = `${size}px`;
-    li.style.height = `${size}px`;
-    li.style.bottom = `-${size}px`;
-    li.style.left = `${position}%`
-    li.style.animationDelay =  `${delay}s`;
-    li.style.animationDuration = `${delay}s`;
-    li.style.borderRadius = '36px';
-    
+  li.style.width = `${size}px`;
+  li.style.height = `${size}px`;
+  li.style.bottom = `-${size}px`;
+  li.style.left = `${position}%`;
+  li.style.animationDelay = `${delay}s`;
+  li.style.animationDuration = `${delay}s`;
+  li.style.borderRadius = "36px";
 
-    li.style.animationTimingFunction = `cubic-bezier(${Math.random()},${Math.random()},${Math.random()},${Math.random()})`
+  li.style.animationTimingFunction = `cubic-bezier(${Math.random()},${Math.random()},${Math.random()},${Math.random()})`;
 
-    bolhas.appendChild(li);
+  bolhas.appendChild(li);
 }
+
+const btnRequest = document.querySelector("#button-request"); //can do better!
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  fetch("pessoas.json")
+    .then((response) => {
+      return response.json();
+    })
+    .then((jsonBody) => {
+      verificaUser(jsonBody);
+    });
+
+  function verificaUser(json) {
+    const inputEmail = document.querySelector("#email");
+    const inputPassword = document.querySelector("#password");
+
+    for (let pessoa of json) {
+      if (inputEmail.value !== pessoa.email && Number(inputPassword.value) !== pessoa.idade) {
+        console.log('a');
+      }
+
+      console.log('cadastrou');
+     
+    }
+  }
+});
